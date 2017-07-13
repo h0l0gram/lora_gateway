737496cd74adca8e28ddca4fa580bcf90893e76f
H0l0 Gram <h0l0gram@users.noreply.github.com>
2017-07-14 00:21:43 +0200

    Added IMST+Omega2 platform

---
+++ libloragw/inc/imst_omega2.h
@@ -0,0 +1,19 @@
+/*
+ * imst_omega2.h
+ *
+ *  Created on: July 07, 2017
+ *      Author: Jan Hänsli
+ */
+
+#ifndef _IMST_OMEGA2_H_
+#define _IMST_OMEGA2_H_
+
+/* Human readable platform definition */
+#define DISPLAY_PLATFORM "IMST + Omega2"
+
+/* parameters for native spi */
+#define SPI_SPEED       8000000
+#define SPI_DEV_PATH    "/dev/spidev32766.1"
+#define SPI_CS_CHANGE   0
+
+#endif /* _IMST_OMEGA2_H_ */
